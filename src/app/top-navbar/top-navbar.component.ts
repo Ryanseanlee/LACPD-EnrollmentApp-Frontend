beacf7882f7e9821142113847740167b42d9701a
joshuaprzx <joshuaprz@hotmail.com>
2022-03-13 23:03:16 -0700

    Changes on everything. More to come

--- src/app/top-navbar/top-navbar.component.ts
+++ src/app/top-navbar/top-navbar.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject , Observable} from 'rxjs';
+import { AdminService } from '../core/services/admin.service';
 
 @Component({
   selector: 'app-top-navbar',
   templateUrl: './top-navbar.component.html',
   styleUrls: ['./top-navbar.component.css']
 })
-export class TopNavbarComponent implements OnInit {
-
-  constructor() { }
+export class TopNavbarComponent {
 
   ngOnInit(): void {
+  }
+
+  isAdminLoggedIn: BehaviorSubject<boolean>;
+
+  constructor(
+    private adminService: AdminService
+  ) {
+    this.isAdminLoggedIn = this.adminService.adminLoggedIn;
+  }
+
+  onLogoutClick(): void {
+    this.adminService.logAdminOut();
   }
 
 }
